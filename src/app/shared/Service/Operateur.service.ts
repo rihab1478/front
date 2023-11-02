9bc80ee40c3b001c56154af37abae93651c5e5f5
rihab nabli <100344175+rihab1478@users.noreply.github.com>
2023-11-02 23:26:40 +0100

    Update Operateur.service.ts

--- src/app/shared/Service/Operateur.service.ts
+++ src/app/shared/Service/Operateur.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/operateur';
+  readonly API_URL = 'http://192.168.1.200:6868/operateur';
 
   constructor(private httpClient: HttpClient) { }
 
